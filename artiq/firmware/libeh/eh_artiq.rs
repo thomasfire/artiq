a5800ae3faeebbdd2f2b726520fd0f409c2e5bcc
Egor Savkin <es@m-labs.hk>
2025-02-12 13:32:14 +0800

    wip make exception message a static buffer
    Signed-off-by: Egor Savkin <es@m-labs.hk>

--- artiq/firmware/libeh/eh_artiq.rs
+++ artiq/firmware/libeh/eh_artiq.rs
@@ -1,6 +1,84 @@
 // ARTIQ Exception struct declaration
 use cslice::CSlice;
-use heapless::String;
+use cslice::{AsCSlice};
+use heapless;
+use core::{mem, str, slice};
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct StringBuffer {
+    pub pos: usize,
+    pub buf: [u8; 128],
+}
+
+impl StringBuffer {
+    /// Copies the given string into the buffer safely.
+    pub fn copy_str(&mut self, s: &str) {
+        let bytes = s.as_bytes();
+        let len = bytes.len().min(self.buf.len().saturating_sub(self.pos));
+
+        self.buf[self.pos..self.pos + len].copy_from_slice(&bytes[..len]);
+        self.pos += len;
+    }
+
+    /// Returns the buffer as a raw byte slice.
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.buf[..self.pos]
+    }
+
+    pub fn as_str(&self) -> &str {
+        if self.pos >= self.buf.len() {
+            "<host string>"
+        } else {
+            str::from_utf8(&self.buf[..self.pos]).unwrap_or("<invalid UTF-8>")
+        }
+    }
+
+    pub fn new() -> Self {
+        StringBuffer {
+            buf: [0; 128],
+            pos: 0,
+        }
+    }
+
+    pub fn from_str(s: &str) -> Self {
+        let mut result: StringBuffer = StringBuffer::new();
+        result.copy_str(s);
+        result
+    }
+
+    pub fn clear(&mut self) {
+        self.pos = 0;
+        self.buf.fill(0);
+    }
+
+    pub fn from_host(message_id: u32) -> Self {
+        let mut result = StringBuffer {
+            buf: [0; 128],
+            pos: usize::MAX,
+        };
+        result.buf[..4].copy_from_slice(unsafe { &mem::transmute::<u32, [u8; 4]>(message_id) });
+        result
+    }
+
+    pub fn is_host(&self) -> bool {
+        self.pos >= 128
+    }
+}
+
+impl core::fmt::Write for StringBuffer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        self.copy_str(s);
+        Ok(())
+    }
+}
+
+impl<'a> AsCSlice<'a, u8> for StringBuffer {
+    fn as_c_slice(&'a self) -> CSlice<'a, u8> {
+        unsafe{CSlice::new((self.buf.as_ptr()), self.pos)}
+    }
+}
+
 
 // Note: CSlice within an exception may not be actual cslice, they may be strings that exist only
 // in the host. If the length == usize:MAX, the pointer is actually a string key in the host.
@@ -12,7 +90,7 @@ pub struct Exception<'a> {
     pub line:     u32,
     pub column:   u32,
     pub function: CSlice<'a, u8>,
-    pub message:  CSlice<'a, u8>
+    pub message:  StringBuffer
 }
 
 fn str_err(_: core::str::Utf8Error) -> core::fmt::Error {
@@ -29,12 +107,21 @@ fn exception_str<'a>(s: &'a CSlice<'a, u8>) -> Result<&'a str, core::str::Utf8Er
 
 impl<'a> core::fmt::Debug for Exception<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "Exception {} from {} in {}:{}:{}, message: {}",
-            self.id,
-            exception_str(&self.function).map_err(str_err)?,
-            exception_str(&self.file).map_err(str_err)?,
-            self.line, self.column,
-            exception_str(&self.message).map_err(str_err)?)
+        if self.message.is_host() {
+            write!(f, "Exception {} from {} in {}:{}:{}, message: {:?}",
+                   self.id,
+                   exception_str(&self.function).map_err(str_err)?,
+                   exception_str(&self.file).map_err(str_err)?,
+                   self.line, self.column,
+                   &self.message.buf[..4])
+        } else {
+            write!(f, "Exception {} from {} in {}:{}:{}, message: {}",
+                   self.id,
+                   exception_str(&self.function).map_err(str_err)?,
+                   exception_str(&self.file).map_err(str_err)?,
+                   self.line, self.column,
+                   exception_str(&self.message.as_str().as_c_slice()).map_err(str_err)?)
+        }
     }
 }
 
